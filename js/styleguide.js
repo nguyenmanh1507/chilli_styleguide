ee09141c26d6248ebdac40ecea2a0fa72bc4e1dd
nguyen manh <nguyenmanh1507@gmail.com>
2016-02-21 17:41:02 +0700

    config angular route

--- js/styleguide.js
+++ js/styleguide.js
@@ -1,4 +1,22 @@
 'use strict';
 
-angular.module('styleGuide', [])
+angular.module('styleGuide', ['ngRoute'])
+	.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/overview', {
+				templateUrl: 'overview.html'
+			})
+			.when('/typography', {
+				templateUrl: 'typography.html'
+			})
+			.otherwise({redirectTo: '/'})
+		;
+
+		if(window.history && window.history.pushState) {
+			$locationProvider.html5Mode({
+				enabled: true,
+				requireBase: false
+			});
+		}
+	})
 ;
